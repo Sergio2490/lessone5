a8ce338e50bfbd486db17f6f28df30b6e5b96e4d
Sergio2490 <merido2490@gmail.com>
2024-10-13 22:48:55 +0300

    add DZob011.py

--- DZ_OB01.py
+++ DZ_OB01.py
@@ -3,22 +3,25 @@
 #Реализуй функцию для добавления задач, отметки выполненных задач и вывода списка
 #текущих (не выполненных) задач.
 class Task:
-    def __init__(self, name, date_end, status):  #инициируем атрибуты
+    def __init__(self, name, date_end, status=False):  #инициируем атрибуты
         self.name = name  #описание задачи
         self.date_end = date_end   #Срок выполнения
         self.status = status   #Статус - вып или не вып.
 
     #Определим функции
+
+    def mark_end(self):  # Отметка о выполнении з-чи
+        self.status = True
+
+     def is_completed(self):  #Проверка статуса
+         return self.status
+
     def add_task(self, task_name, task_date):  #добавление новой задачи
         self.name = task_name
         self.date_end = task_date  #Дата, до которой нужно исполнить
         self.status = False
 
-    def mark_end(self):  #Отметка о выполнении з-чи (или изменение статуса)
-        if self.status == False:
-            self.status = True
-        elif self.status == True:
-            self.status = False
+
 
     def list_true_tasks(self):  #Вывод невыполненных задач
         if self.status == False:
